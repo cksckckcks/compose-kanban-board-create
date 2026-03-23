0a2b52bf07bad87716203a2c917913aae8bdbebb
cksckckcks <chan679999@naver.com>
2026-03-23 16:20:57 +0900

    refactor: 프리뷰 파라미터 적용

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/dialog/component/TaskFieldLabel.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/dialog/component/TaskFieldLabel.kt
@@ -5,6 +5,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -22,15 +24,18 @@ fun TaskFieldLabel(
     )
 }
 
+private class TaskFieldLabelParameterProvider : PreviewParameterProvider<Pair<String, Boolean>> {
+    override val values = sequenceOf(
+        "제목" to true,
+        "설명" to false,
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
-private fun TaskFieldLabelPreview() {
-    Column {
-        TaskFieldLabel(
-            label = "제목",
-            isRequired = true,
-        )
-
-        TaskFieldLabel(label = "설명")
-    }
+private fun TaskFieldLabelPreview(@PreviewParameter(TaskFieldLabelParameterProvider::class) params: Pair<String, Boolean>) {
+    TaskFieldLabel(
+        label = params.first,
+        isRequired = params.second,
+    )
 }
